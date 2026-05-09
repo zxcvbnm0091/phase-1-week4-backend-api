71b77ef7128d61974a4a78571f46d5e7740af883
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-05-09 14:00:27 +0700

    feat: add the rest of auth routes

--- logic_nolep/arcane-quest/fancy-todo-prisma/src/routes/authRoutes.js
+++ logic_nolep/arcane-quest/fancy-todo-prisma/src/routes/authRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-
+import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
-import { register, login } from "../controllers/authController.js";
+import {
+  register,
+  login,
+  logout,
+  deleteUser,
+} from "../controllers/authController.js";
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", protect, logout);
+router.delete("/deleteUser", protect, deleteUser);
 
 export default router;
