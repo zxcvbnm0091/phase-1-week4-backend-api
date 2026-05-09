32e953f8a88b25cd2c42463f7cee1f3944a962a7
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-05-09 13:58:30 +0700

    fix: fix login and logout response

--- logic_nolep/arcane-quest/fancy-todo-prisma/src/controllers/authController.js
+++ logic_nolep/arcane-quest/fancy-todo-prisma/src/controllers/authController.js
@@ -30,7 +30,7 @@ const register = async (req, res) => {
           },
         },
       },
-      // Use 'select' to grab specific fields from both User and Profile
+
       select: {
         id: true,
         email: true,
@@ -71,51 +71,49 @@ const login = async (req, res) => {
       return res.status(401).json({ error: "Invalid credentials" });
     }
 
-    // --- ADDED THIS PART ---
-    const jwtToken = sendTokenResponse(user, 200, res);
-
-    res.json({
-      message: "Login Success",
-      status: "success",
-      user: {
-        id: user.id,
-        email: user.email,
-      },
-      jwtToken, // Send the token back to the frontend
-    });
-    // -----------------------
+    return sendTokenResponse(user, 200, res);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
 const logout = (req, res) => {
-  res.cookie("token", "none", {
-    expires: new Date(Date.now() + 10 * 1000), // Expire in 10 seconds
+  res.cookie("token", "", {
+    expires: new Date(0),
     httpOnly: true,
+    secure: process.env.PROCESS_ENV === "production",
+    sameSite: "strict",
   });
 
   res.status(200).json({ success: true, message: "Logged out" });
 };
 
 const deleteUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await prisma.user.findUnique({
-      where: { id: id },
-    });
+  const userId = req.user.id;
+  console.log("User Id", userId);
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
 
+  try {
     if (!user)
       return res.status(404).json({
         error: "user not found",
       });
 
     const deleteUser = await prisma.user.delete({
-      where: { id: id },
+      where: { id: userId },
+    });
+
+    res.cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
     });
 
     res.status(200).json({
-      message: "User Deleted",
+      message: "User and all associated data deleted successfully",
       status: "success",
     });
   } catch (error) {
