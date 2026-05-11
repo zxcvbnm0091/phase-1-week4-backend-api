e3613c72ed8652bc9cffa08493612c2adb0945a1
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-05-11 20:39:27 +0700

    fix: fix route

--- logic_nolep/arcane-quest/fancy-todo-mangoose/src/app.js
+++ logic_nolep/arcane-quest/fancy-todo-mangoose/src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import allRoutes from "./routes/index.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.use("/api", routes);
+app.use("/api", allRoutes);
 
 export default app;
