359ac9c67acbfd13531628f1e09f226086529123
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-05-11 20:44:18 +0700

    feat: add all routes

--- logic_nolep/arcane-quest/fancy-todo-mangoose/src/routes/index.js
+++ logic_nolep/arcane-quest/fancy-todo-mangoose/src/routes/index.js
@@ -1,7 +1,14 @@
 import express from "express";
-import userRoute from "./user.route";
-import todoRoute from "./todo.route";
+import userRoute from "./user.route.js";
+import todoRoute from "./todo.route.js";
+import profileRoute from "./profile.route.js";
+import authRoute from "./auth.route.js";
+
 const router = express.Router();
 
+router.use("/auth", authRoute);
 router.use("/user", userRoute);
-router.use("/todo", todoRoute);
+router.use("/todos", todoRoute);
+router.use("/profile", profileRoute);
+
+export default router;
