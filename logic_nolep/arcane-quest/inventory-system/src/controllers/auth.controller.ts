45b732d75e5db4fdb5a730559b43969b09da9cb6
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-05-15 16:18:33 +0700

    feat: add auth controller

---
+++ logic_nolep/arcane-quest/inventory-system/src/controllers/auth.controller.ts
@@ -0,0 +1,53 @@
+import * as authService from "../service/auth.service";
+import type { Request, Response } from "express";
+import type { LoginDto, RegisterDto } from "../dtos/auth.dto";
+import type { JWTPayload } from "../types";
+import { sendTokenResponse } from "../utils/JWTToken";
+
+class AuthController {
+  static async login(req: Request, res: Response) {
+    try {
+      const user = await authService.login(req.body as LoginDto);
+
+      const payload: JWTPayload = {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      };
+
+      sendTokenResponse(payload, 200, res);
+    } catch (error: any) {
+      res.status(error.statusCode ?? 500).json({
+        error: error.message,
+      });
+    }
+  }
+
+  static async register(req: Request, res: Response) {
+    try {
+      const user = await authService.register(req.body as RegisterDto);
+
+      const payload: JWTPayload = {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+      };
+
+      sendTokenResponse(payload, 201, res);
+    } catch (error: any) {
+      res.status(error.statusCode ?? 500).json({ error: error.message });
+    }
+  }
+
+  static async logout(req: Request, res: Response) {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+    });
+
+    res.status(200).json({ message: "Logged out success", success: true });
+  }
+}
+
+export default AuthController;
