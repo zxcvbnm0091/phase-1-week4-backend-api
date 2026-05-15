433efffcdc95a3384fd7bba876eea2fd3c27f47c
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-05-15 16:19:11 +0700

    feat: add auth route

---
+++ logic_nolep/arcane-quest/inventory-system/src/routes/auth.route.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import AuthController from "../controllers/auth.controller";
+import validate from "../middlewares/validate";
+import { protect } from "../middlewares/auth.middleware";
+import { RegisterSchema, LoginSchema } from "../dtos/auth.dto";
+
+const router = express.Router();
+
+router.post("/register", validate(RegisterSchema), AuthController.register);
+router.post("/login", validate(LoginSchema), AuthController.login);
+router.post("/delete", protect, AuthController.logout);
+
+export default router;
