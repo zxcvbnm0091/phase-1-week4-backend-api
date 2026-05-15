c54d945d5f52392af521d19915178f77dd576a88
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-05-15 16:19:27 +0700

    feat: add auth route path

--- logic_nolep/arcane-quest/inventory-system/src/routes/index.route.ts
+++ logic_nolep/arcane-quest/inventory-system/src/routes/index.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import userRoutes from "./user.route";
+import authRoutes from "./auth.route";
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
+router.use("/auth", authRoutes);
 
 export default router;
