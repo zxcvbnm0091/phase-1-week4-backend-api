eab984bea646738696835982785ed626fa7dfbfd
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-05-15 16:18:15 +0700

    fix: update api routes

--- logic_nolep/arcane-quest/inventory-system/src/app.ts
+++ logic_nolep/arcane-quest/inventory-system/src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/user.route";
+import Apiroutes from "./routes/index.route";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/users", userRoutes);
+app.use("/api", Apiroutes);
 
 export default app;
