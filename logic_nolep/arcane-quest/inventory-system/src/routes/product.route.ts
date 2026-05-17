01aed3c31045d7b47c839a6f06627c3ae1d84c3e
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-05-17 23:49:50 +0700

    feat: add product route

---
+++ logic_nolep/arcane-quest/inventory-system/src/routes/product.route.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import productController from "../controllers/product.controller";
+import validate from "../middlewares/validate";
+import { CreateProductSchema, UpdateProductSchema } from "../dtos/product.dto";
+import { protect } from "../middlewares/auth.middleware";
+const router = express.Router();
+
+router.get("/all", protect, productController.getAllProduct);
+router.get("/:id", protect, productController.getProductById);
+
+router.post(
+  "/",
+  protect,
+  validate(CreateProductSchema),
+  productController.createProduct,
+);
+
+router.patch(
+  "/:id",
+  protect,
+  validate(UpdateProductSchema),
+  productController.updateProduct,
+);
+
+router.post("/delete/:id", protect, productController.deleteProduct);
