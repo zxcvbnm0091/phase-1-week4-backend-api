25457f1539f49583be74aae864c010b36fb1bfb2
zxcvbnm0091 <nisrinaadil45@gmail.com>
2026-05-19 10:53:46 +0700

    refactor: centralized env with config

--- logic_nolep/arcane-quest/inventory-system/src/lib/prisma.ts
+++ logic_nolep/arcane-quest/inventory-system/src/lib/prisma.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
+import config from "../config/config";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = config.database.url;
 
 if (!connectionString) {
   throw new Error("DATABASE_URL environment variable is not defined");
